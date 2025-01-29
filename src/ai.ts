89bb1218bff0eaff09cccc43e365f4f06c95f6d1
Gilmarllen <gilmarllen@gmail.com>
2025-01-28 22:59:48 -0300

    Add new fields to the AI prompt

--- src/ai.ts
+++ src/ai.ts
@@ -1,5 +1,7 @@
 import { PostData } from './gymrat/types'
 import { GoogleGenerativeAI } from '@google/generative-ai'
+import { format, parseISO } from 'date-fns'
+import { pt } from 'date-fns/locale'
 
 function fieldInfo(
   field: string,
@@ -12,14 +14,23 @@ function fieldInfo(
   return ''
 }
 
+function formatDate(dateString: string) {
+  const date = parseISO(dateString)
+  return format(date, "d 'de' MMMM 'de' yyyy, HH:mm", { locale: pt })
+}
+
 export function formatPrompt(post: PostData) {
   return `
     Crie uma resposta zoeira para o post/log de atividade física com as seguintes informações:
     ${fieldInfo('Título', post.title)}
     ${fieldInfo('Descrição', post.description)}
-    ${fieldInfo('Duração em minutos', post.duration)}
+    ${fieldInfo('Duração em minutos', post.formatted_details.duration)}
+    ${fieldInfo('Calorias', post.formatted_details.calories)}
+    ${fieldInfo('Passos', post.formatted_details.steps)}
+    ${fieldInfo('Distância', post.formatted_details.distance)}
     ${fieldInfo('Usuário', post.account.full_name.split(' ')[0])}
     ${fieldInfo('Mídia(s)', post.media.map(({ url }) => url).join(', '))}
+    ${fieldInfo('Ocorrido em', formatDate(post.occurred_at))}
 
     A resposta deve ser única e será diretamente redirecionada como comentário no post.
   `
